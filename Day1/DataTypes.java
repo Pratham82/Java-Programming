d73df3a024a19c96f2f82cb0cb73f6c2d51e3747
Pratham82 <mali.prathamesh82@gmail.com>
2019-12-25 16:16:04 +0530

    Day 1 Files for Java course

---
+++ Day1/DataTypes.java
@@ -0,0 +1,18 @@
+package Day1;
+
+public class DataTypes {
+    public static void main (String args[]) {
+        byte b = 123;
+        short c = 12345;
+        int d = 1234567891;
+        long e = 1234567891;
+        float f = 45.5f;
+        double g = 789.50;
+        System.out.println("Value of b = "+ b);
+        System.out.println("Value of c = "+ c);
+        System.out.println("Value of d = "+ d);
+        System.out.println("Value of e = "+ e);
+        System.out.println("Value of f = "+ f);
+        System.out.println("Value of g = "+ g);
+    }
+}
---
+++ Day1/NewLines.java
@@ -0,0 +1,35 @@
+package Day1;
+
+public class NewLines {
+    public static void  main (String[]args){
+        System.out.println("Hi this is Prathamesh");
+        System.out.println("Hi this ");
+        System.out.println("is Prathamesh");
+        System.out.print("Hi this");
+        System.out.print("is Prathamesh");
+        System.out.println("\n"); //  \n Used for new lines
+        System.out.println("Hi this is\tPrathamesh"); //  \t used for tab
+
+        //Printing 1 2 3 4 5 in horizontal and vertically
+        System.out.println("\nPrinting 1 2 3 4 5 in horizontal and vertically:\n");
+        //Method 1
+        System.out.println("1 2 3 4 5");
+        System.out.println("\n");
+        System.out.println("1\n2\n3\n4\n5\n");
+
+        //Method 2
+        System.out.print("1 ");
+        System.out.print("2 ");
+        System.out.print("3 ");
+        System.out.print("4 ");
+        System.out.print("5 ");
+
+        System.out.println("\n1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+
+
+    }
+}
