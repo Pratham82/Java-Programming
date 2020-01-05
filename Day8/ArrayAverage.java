25cf2568ae57416ef205a97a96b898e7cb8a1e14
Pratham82 <mali.prathamesh82@gmail.com>
2020-01-05 23:02:34 +0530

    Day 8 update

---
+++ Day8/ArrayAverage.java
@@ -0,0 +1,25 @@
+package Dya8;
+import java.util.Scanner;
+
+public class ArrayAverage{
+
+public static void main(String []args){
+	Scanner scan = new Scanner(System.in);
+	int A[] = new int [5];
+        int total =0;
+        double avg =0;
+        
+	System.out.println("Enter 5 values to find out average:");
+	
+	for (int i =0; i<A.length; i++){
+	     A[i] = scan.nextInt();
+	     total = total +  A[i];
+           }
+    
+          avg = total / A.length;
+
+	  System.out.println("Average of the entered numbers is: "+ avg);
+	  
+	  }
+}
+          
