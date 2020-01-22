2600dcec73ec151c08b6fb8f702deb4cb82727ac
Pratham82 <mali.prathamesh82@gmail.com>
2020-01-22 17:25:07 +0530

    Packages implementation

---
+++ Packages/Demo2/Bike.java
@@ -0,0 +1,10 @@
+package Packages.Demo2;
+
+import Packages.Demo1.Vehicle;
+
+public class Bike extends Vehicle {
+    public static void main(String[] args) {
+        Vehicle v= new Vehicle();
+        v.Show();
+    }
+}
---
+++ Packages/Demo2/Test.java
@@ -0,0 +1,11 @@
+package Packages.Demo2;
+
+import Packages.Demo1.Vehicle;
+
+public class Test{
+    public static void main(String[] args) {
+        Vehicle v = new Vehicle();
+        v.Show();
+    }
+}
+
