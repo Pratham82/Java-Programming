c9136c8e7eb69123b35177a6d1cc2e41bae8116f
Pratham82 <mali.prathamesh82@gmail.com>
2020-01-22 17:36:25 +0530

    Files deleted

--- Day19/Demo1/Car.java
+++
@@ -1,9 +0,0 @@
-package Day19.Demo1;
-
-public class Car extends Vehicle {
-	public static void main(String []args){
-		Vehicle v= new Vehicle();
-		v.Drive();
-
-	}
-}
--- Day19/Demo1/TestVehicle.java
+++
@@ -1,8 +0,0 @@
-package Day19.Demo1;
-
-public class TestVehicle{
-    public static void main(String[] args) {
-        Vehicle v = new Vehicle();
-        v.Drive();
-    }
-}
