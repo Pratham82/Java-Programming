a7f83a5af283f42693fd88b1e4ca7f6e3cfd2f79
Pratham82 <mali.prathamesh82@gmail.com>
2020-01-24 18:25:25 +0530

    Sorting strings

---
+++ Practice_Problems/StringSorting.java
@@ -0,0 +1,32 @@
+package Pracrtice_Problems;
+
+import java.util.Scanner;
+
+public class StringSorting{
+	public static void main(String [] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter desired lentgh for the array: ");
+		int ALength = scan.nextInt();
+		System.out.println("Enter "+ALength+" words: ");
+		String A[] = new String [ALength];
+		String temp = null;
+
+		for(int i=0; i<A.length; i++){
+			A[i]= scan.next();
+		}
+
+		System.out.println("Enter soreted list ");
+		
+		for(int i =0; i<A.length; i++){
+			for(int j = i+1; j<A.length; j++){
+				if(A[i].compareTo(A[j])>0){
+					temp = A[i];
+					A[i] = A[j];
+					A[j] = temp;
+				}
+			}
+			System.out.println(A[i]);
+		}
+	}
+}
+
