cca9dbd6a413514c9085d64f5b2d6bddc0cd8ca1
Pratham82 <mali.prathamesh82@gmail.com>
2020-01-24 18:50:59 +0530

    Reverse string

---
+++ Practice_Problems/ReversingString.java
@@ -0,0 +1,21 @@
+package Practice_Problems;
+
+import java.util.Scanner;
+
+public class ReversingString{
+	public static void main(String [] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter string for reversing: ");
+		String file = scan.next();
+		String revFile = "";
+
+		for(int i = file.length()-1; i>=0; i--){
+			revFile = revFile + file.charAt(i);
+		}
+		System.out.println("Entered string: "+file);
+		System.out.println("Reversed string: "+revFile);
+
+	}
+}
+		
+
