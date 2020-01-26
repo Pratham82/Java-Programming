f39d22b1e6e50922d10038f9ec434253fbebaef8
Pratham82 <mali.prathamesh82@gmail.com>
2020-01-26 13:40:15 +0530

    Practice problems from HeadFirst

---
+++ Practice_Problems/DrumKit.java
@@ -0,0 +1,34 @@
+package Practice_Problems;
+
+class DrumKit {
+
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+
+    void playTopHat(){
+        System.out.println("ding ding da-ding");
+    }
+
+}
+
+class DrumKitTestDrive {
+    public static void main(String [] args) {
+        DrumKit d = new DrumKit();
+
+        d.playTopHat();
+
+        d.playSnare();
+
+        d.snare = false;
+
+        if (d.snare) {
+            d.playSnare();
+        }
+
+
+    }
+}
---
+++ Practice_Problems/ExerciseB.java
@@ -0,0 +1,13 @@
+package Practice_Problems;
+
+class Exercise1b {
+    public static void main(String [] args) {
+        int x = 1;
+        while ( x < 10 ) {
+            x = x + 1;
+            if ( x > 3) {
+                System.out.println("big x");
+            }
+        }
+    }
+}
