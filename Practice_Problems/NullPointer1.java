c778e123421926d959781ab3fc92cce74526ab0b
Pratham82 <mali.prathamesh82@gmail.com>
2020-01-29 21:15:58 +0530

    Null pointer program updated

--- Practice_Problems/NullPointer1.java
+++ Practice_Problems/NullPointer1.java
@@ -1,22 +1,23 @@
 package Practice_Problems;
 
 public class NullPointer1 {
-    static public  void main(String[] args) {
+    public static void main(String[] args) {
     NullPointer1 np = new NullPointer1();
     String c = null;
-    String[] d = {};
-    np.method(np.a);
-        //System.out.println(c.length());
-    np.Null1();
-        System.out.println(d.length);
+
+    String d = "hi";
+
+    np.method(np.b);
+    System.out.println(d);
+    System.out.println(np.b);
+    //System.out.println(c.length()); will give nullPointer exception
     }
 
     String a = null;
     String b = "b variable";
+
     void method(String val){
         System.out.println(val);
     }
-    String Null1(){
-        return null;
-    }
+
 }
