d0a22deb58522ac96dad33e29a8a2c465b82c399
Pratham82 <mali.prathamesh82@gmail.com>
2020-03-24 13:03:02 +0530

    Cleaned up some code in Assignment 6

--- Assignment_6/Question3.java
+++ Assignment_6/Question3.java
@@ -5,7 +5,9 @@ public class Question3 {
     public static void main(String[] args) {
         String s1 =  "Sachin Ramesh Tendulkar";
         String [] s2 = s1.split(" ");
-        for(int i = 0; i<=s2.length-2; i++){
+        System.out.println("Original string: "+s1);
+        System.out.print("New string: ");
+        for(int i = 0; i<s2.length-1; i++){
             System.out.print(s2[i].charAt(0)+".");
         }
         //System.out.println(s2[s2.length-1]);
