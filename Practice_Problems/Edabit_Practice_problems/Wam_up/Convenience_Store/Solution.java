bb8492c3b90791153068ccb475f46c9525111c86
pratham82 <mali.prathamesh82@gmail.com>
2021-07-12 23:07:10 +0530

    Added new easy questions

---
+++ Practice_Problems/Edabit_Practice_problems/Wam_up/Convenience_Store/Solution.java
@@ -0,0 +1,2 @@
+package Practice_Problems.Edabit_Practice_problems.Wam_up.Convenience_Store;public class Solution {
+}
---
+++ Practice_Problems/Edabit_Practice_problems/Wam_up/How_much_is_true/Solution.java
@@ -0,0 +1,4 @@
+package Practice_Problems.Edabit_Practice_problems.Wam_up.How_much_is_true;
+
+public class Solution {
+}
