fe56879394e8e06bdf382fb271dc5510cc6fbed5
pratham82 <mali.prathamesh82@gmail.com>
2021-07-14 23:37:17 +0530

    Added mooc exercises solution

---
+++ Mooc.fi problems/part01-Part01_05.Message/src/main/java/Message.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        
+        // Write your program here
+        
+        // Taking the input
+        String message = scanner.nextLine();
+        
+        // Print the message
+        System.out.println(message);
+
+    }
+}
