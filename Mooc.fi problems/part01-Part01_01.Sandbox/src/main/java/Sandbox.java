11c96c67117073b503d42fe680f80f27b3559651
pratham82 <mali.prathamesh82@gmail.com>
2021-07-16 23:36:18 +0530

    Removed mooc values

--- Mooc.fi problems/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++
@@ -1,8 +0,0 @@
-
-public class Sandbox {
-
-    public static void main(String[] args) {
-        // Write your program here
-        System.out.println("Hello, World");
-    }
-}
--- Mooc.fi problems/part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
+++
@@ -1,8 +0,0 @@
-
-public class HiAdaLovelace {
-
-    public static void main(String[] args) {
-        String name = "Ada Lovelace";
-        System.out.println("Hi "+name+"!");
-    }
-}
